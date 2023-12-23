19aad6a840e419292004cc7a179a8e5f74775388
sherzamon <sh.muhiddinov@uic.group>
2023-12-24 03:11:46 +0500

    wdew

---
+++ bot_apps/management/commands/parser.py
@@ -0,0 +1,28 @@
+import asyncio
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from client.models import Filename
+from setting_ads.models import Channels
+from setting_ads.models import Client_Settings
+import requests
+
+
+def send_to_telegram(bot_token, chat_id):
+    caption= f"Ads_manager Date: {timezone.now()}"
+    url = f"https://api.telegram.org/bot{bot_token}/sendDocument"
+    files = {'document': open("./mydatabase.sqlite3", 'rb')}
+    data = {'chat_id': chat_id, 'caption': caption} if caption else {'chat_id': chat_id}
+    response = requests.post(url, files=files, data=data)
+    return response.json()
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        help = 'Starts the Telegram listener'
+        bot_token = "6607374521:AAHjnBaqbDqJKvFkF_HsiJ6losu50j6vHiE"
+        chat_id = -1002041724232
+        send_to_telegram(bot_token, chat_id)
+
+
