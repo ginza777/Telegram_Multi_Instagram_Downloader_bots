1c4050390bb86b23be63e9247908ee8a6abb1bd7
sherzamon <sh.muhiddinov@uic.group>
2023-12-24 01:29:16 +0500

    wdew

--- core/settings.py
+++ core/settings.py
@@ -88,11 +88,7 @@ DATABASES = {
     "default": {
         "ENGINE": env.str("DB_ENGINE"),
         "NAME": env.str("DB_NAME"),
-        "USER": env.str("DB_USER"),
-        "PASSWORD": env.get_value("DB_PASSWORD"),
-        "HOST": env.str("DB_HOST"),
-        "PORT": env.str("DB_PORT"),
-        "ATOMIC_REQUESTS": True,
+
     }
 }
 
